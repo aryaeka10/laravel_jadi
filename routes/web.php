3b600343cb7fecf0d60de6ba96db16767ebf6c33
agungarr <agungarrafi9@gmail.com>
2021-03-01 15:10:56 +0700

    Pertemuan 4

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\MemberController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+//Route::get('/users', 'UserController@index');
+Route::get('/users', [UserController::class, 'index']);
+
+Route::resource('/members', [MemberController::class]);
